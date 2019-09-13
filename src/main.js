6c118b609ecee040fd84a6a98d0827304258c20e
nikhilbatta <nbatta08@gmail.com>
2019-09-13 15:22:26 -0700

    adds adresses for doctors offices

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {SearchForDoctor} from './doctorsearch.js'
 import $ from 'jquery'
-
+import './styles.css';
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
@@ -24,8 +24,9 @@ function displayDataAsTable(response){
     const doctors = JSON.parse(response);
     console.log(doctors)
     doctors.data.forEach(function(doctor){
-      console.log(doctor.profile)
-      $(".firstName").append(`<li> ${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.profile.bio} <br> <img src=${doctor.profile.image_url} </img> <br> </li>`)
+      console.log(doctor.practices[0].visit_address.street)
+
+      $(".firstName").append(`<li> ${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.profile.bio} <br> <img src=${doctor.profile.image_url}  </img> <br> My address: ${doctor.practices[0].visit_address.street} <br> I'm willing to accept new patients:${doctor.practices[0].accepts_new_patients} </li> <br> <br>`)
       // $(".lastName").append(`<li> ${doctor.profile.last_name} </li>`)
     })
 }
