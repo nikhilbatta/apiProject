8e80304eb21432d8cd475abb8ab63664e74063f4
nikhilbatta <nbatta08@gmail.com>
2019-09-13 12:21:17 -0700

    add query for searching by doctor name

--- src/doctorsearch.js
+++ src/doctorsearch.js
@@ -1,8 +1,9 @@
 export class SearchForDoctor {
-  getDoctorByCondition(healthcondition) {
+  getDoctorByCondition(doctorFirstName,healthcondition) {
     return new Promise(function(resolve,reject){
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${healthcondition}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&gender=male&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${doctorFirstName}&query=${healthcondition}&location=OR&user_location=45.5051%2C122.6750&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+      console.log(url)
       request.onload = function(){
         if(this.status === 200){
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -5,15 +5,18 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     const healthcondition = $("#healthcondition").val();
-    callDoctorAPI(healthcondition)
+    const doctorFirstName = $("#doctorName").val();
+    console.log(doctorFirstName)
+    console.log(healthcondition)
+    callDoctorAPI(doctorFirstName, healthcondition)
     .then(function(response){
       const doctors = JSON.parse(response);
       console.log(doctors)
     })
   })
 })
-function callDoctorAPI(healthcondition){
+function callDoctorAPI(doctorFirstName,healthcondition){
   let doctorByCondition = new SearchForDoctor();
-  let promise = doctorByCondition.getDoctorByCondition(healthcondition)
+  let promise = doctorByCondition.getDoctorByCondition(doctorFirstName, healthcondition)
   return promise;
 }
