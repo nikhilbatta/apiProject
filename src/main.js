c5a686b09f5cc608e7cdb9292ca8c412c5d6a15c
nikhilbatta <nbatta08@gmail.com>
2019-09-13 13:56:59 -0700

    makes changes to the data displayed to UI

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 })
 
 function getAndDisplayData(){
-  const healthcondition = 8;
+  const healthcondition = $("#healthcondition").val()
   const doctorFirstName = $("#doctorName").val();
   callDoctorAPI(doctorFirstName, healthcondition).then(displayDataAsTable, displayError);
 }
@@ -22,12 +22,16 @@ function callDoctorAPI(doctorFirstName,healthcondition){
 
 function displayDataAsTable(response){
     const doctors = JSON.parse(response);
+    console.log(doctors)
     doctors.data.forEach(function(doctor){
       console.log(doctor.profile)
-      $(".firstName").append(`<li> ${doctor.profile.first_name} ${doctor.profile.last_name} </li>`)
+      $(".firstName").append(`<li> ${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.profile.bio} <br> <img src=${doctor.profile.image_url} </img> <br> </li>`)
       // $(".lastName").append(`<li> ${doctor.profile.last_name} </li>`)
     })
 }
 function displayError(response){
     console.log(response.statusText);
 }
+// function clearPreviousSearch(){
+//   $(".firstName").append("");
+// }
