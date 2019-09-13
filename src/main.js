f2adb2e7b37ae6ebf889772f03db4a64b352da4d
nikhilbatta <nbatta08@gmail.com>
2019-09-13 13:21:43 -0700

    refactoring code to be cleaner with single functionality

--- src/main.js
+++ src/main.js
@@ -4,19 +4,33 @@ import $ from 'jquery'
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    const healthcondition = $("#healthcondition").val();
-    const doctorFirstName = $("#doctorName").val();
-    console.log(doctorFirstName)
-    console.log(healthcondition)
-    callDoctorAPI(doctorFirstName, healthcondition)
-    .then(function(response){
-      const doctors = JSON.parse(response);
-      console.log(doctors)
-    })
+    getAndDisplayData();
   })
 })
+
+function getAndDisplayData(){
+  const healthcondition = 8;
+  const doctorFirstName = $("#doctorName").val();
+  callDoctorAPI(doctorFirstName, healthcondition).then(displayDataAsTable, displayError);
+}
+
 function callDoctorAPI(doctorFirstName,healthcondition){
   let doctorByCondition = new SearchForDoctor();
   let promise = doctorByCondition.getDoctorByCondition(doctorFirstName, healthcondition)
   return promise;
+}
+
+function displayDataAsTable(response){
+    const doctors = JSON.parse(response);
+    doctors.data.forEach(function(doctor){
+      console.log(doctor.profile)
+    })
+
+}
+function displayError(response){
+    // const doctors = JSON.parse(response);
+    // doctors.data.forEach(function(doctor){
+    //   console.log(doctor.profile)
+    // })
+    console.log(response);
 }
